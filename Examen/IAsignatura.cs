775207e0e44cf9ffe5eea5e8538699f0de8a3954
fredybel <fredybeltran712@gmail.com>
2024-11-06 19:04:41 -0600

    Creando la interfaz/parte3

---
+++ Examen/IAsignatura.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    public interface IAsignatura
+    {
+        public double CalcularNotaFinal();
+        public double CalcularNotaFinal();
+        public string MensajeNotaFinal(double NotaFinal);
+        public void Imprimir();
+    }
