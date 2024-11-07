10bf9f7ab98c99cc1e72a0b3493dfe5ea6fd7df0
fredybel <fredybeltran712@gmail.com>
2024-11-07 12:17:56 -0600

    Impresiones listas

--- Examen/Asignatura.cs
+++ Examen/Asignatura.cs
@@ -11,19 +11,40 @@ namespace Examen
     {
         public int N1 { get; set; }
         public int N2 { get; set; }
-        public string N3 { get; set; }
+        public int N3 { get; set; }
         public string NombreAsignatura { get; set; }
         public string Horario { get; set; }
         public string NombreDocente { get; set; }
 
 
+
         public double CalcularNotaFinal()
         {
-            throw new NotImplementedException();
+            return N1 + N2 + N3;
         }
 
+        public double CalcularNotaFinal(double N1, double N2, double N3)
+        {
+            return N1 + N2 + N3;
+        }
+
+
+
+        public string MensajeNotaFinal(double NotaFinal)
+        {
+               
+                if (NotaFinal < 60)
+                    return "Reprobado";
+                if (NotaFinal < 80)
+                    return "Bueno";
+                if (NotaFinal < 90)
+                    return "Muy Bueno";
+                return "Sobresaliente";
+            
+        }
         public void Imprimir()
         {
+            double NotaFinal = CalcularNotaFinal();
             Console.WriteLine();
             Console.WriteLine("***********************************************");
             Console.WriteLine($"Nombre del estudiante: {NombreAlumno}");
@@ -32,13 +53,13 @@ namespace Examen
             Console.WriteLine($"Nombre de la clase:: {NombreAsignatura}");
             Console.WriteLine($"Horario: {Horario}");
             Console.WriteLine($"Nombre del docente: {NombreDocente}");
+            Console.WriteLine($"Nota Final:: {CalcularNotaFinal()}");
+            Console.WriteLine($"Nombre del docente: {NombreDocente}");
+            Console.WriteLine($"Nota Final:: {NotaFinal}-{MensajeNotaFinal(NotaFinal)}");
 
+            
         }
 
-
-        public string MensajeNotaFinal(double NotaFinal)
-        {
-            throw new NotImplementedException();
-        }
+       
     }
 }
--- Examen/IAsignatura.cs
+++ Examen/IAsignatura.cs
@@ -9,7 +9,7 @@ namespace Examen
     public interface IAsignatura
     {
         public double CalcularNotaFinal();
-        
+        public double CalcularNotaFinal(double N1, double N2, double N3 );
         public string MensajeNotaFinal(double NotaFinal);
         public void Imprimir();
     }
