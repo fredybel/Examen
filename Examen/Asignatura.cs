e26acce3072d3a6d7b522c6c1fdfb56b7773537d
fredybel <fredybeltran712@gmail.com>
2024-11-07 01:35:37 -0600

    Update Asignatura.cs

--- Examen/Asignatura.cs
+++ Examen/Asignatura.cs
@@ -9,4 +9,21 @@ namespace Examen
     internal class Asignatura
     {
     }
-}
+
+namespace Examen
+{
+        //CLASE HIJA
+        public class Asignatura : Alumno, IAsignatura//HERENCIA
+        {
+            public int N1 { get; set; }
+            public int N2 { get; set; }
+            public string N3 { get; set; }
+            public string NombreAsignatura { get; set; }
+            public string Horario { get; set; }
+            public string NombreDocente { get; set; }
+
+
+            public double CalcularNotaFinal()
+            {
+                throw new NotImplementedException();
+            }
